c72e949615d748fa6485711c3b203d3c17f5f113
Daniel Costa Carvalho Martins <danielcostacarvalhomartins06@gmail.com>
2025-11-23 22:12:38 -0300

    migrations

--- back/prisma/migrations/20251005220858_init/migration.sql
+++ back/prisma/migrations/20251005220858_init/migration.sql
@@ -1,4 +1,14 @@
 -- CreateTable
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ativo BOOLEAN DEFAULT TRUE
+);
+
 CREATE TABLE "Tarefa" (
     "id" SERIAL NOT NULL,
     "titulo" TEXT NOT NULL,
